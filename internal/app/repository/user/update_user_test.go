f3aa0709c18d4011263428f164c707518a3a2dc6
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-15 19:45:20 +0700

    update user repository

--- internal/app/repository/user/update_user_test.go
+++ internal/app/repository/user/update_user_test.go
@@ -3,6 +3,7 @@ package user
 import (
 	"testing"
 
+	"github.com/HemlockPham7/common-libs/pkg/dbutils"
 	"github.com/HemlockPham7/user-service/internal/app/model"
 	"github.com/HemlockPham7/user-service/internal/integration_test/data/fixtures"
 	"github.com/stretchr/testify/assert"
@@ -23,7 +24,7 @@ func TestSqlRepository_UpdateUser(t *testing.T) {
 		expectedError error
 	}{
 		{
-			name: "normal case",
+			name: "Update user by ID successfully",
 
 			setupDB: func(t *testing.T) *gorm.DB {
 				return fixtures.NewFixture(t, &fixtures.UserCommonTestDB{})
@@ -35,6 +36,38 @@ func TestSqlRepository_UpdateUser(t *testing.T) {
 				Email:       "userupdated@gmail.com",
 			},
 			expectedError: nil,
+		},
+		{
+			name: "Update user by ID failed - user not found",
+
+			setupDB: func(t *testing.T) *gorm.DB {
+				return fixtures.NewFixture(t, &fixtures.UserCommonTestDB{})
+			},
+
+			inputID: "non-existent-id",
+
+			inputUserData: &model.User{
+				DisplayName: "Non Existent User",
+				Email:       "",
+			},
+
+			expectedError: dbutils.ErrRecordNotFoundType,
+		},
+		{
+			name: "Update user by ID failed - duplicate email",
+
+			setupDB: func(t *testing.T) *gorm.DB {
+				return fixtures.NewFixture(t, &fixtures.UserCommonTestDB{})
+			},
+
+			inputID: "d7c13097-67a7-4eae-a60e-0b9b533b7b10",
+
+			inputUserData: &model.User{
+				DisplayName: "Alice",
+				Email:       "janedoe@gmail.com", // duplicate email
+			},
+
+			expectedError: dbutils.ErrDuplicationType,
 		},
 	}
 
